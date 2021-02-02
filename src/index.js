ec1e6a3d641839dcead74ab992f0f6da6dd90d6d
Gabriel Gonzalez <hgglez92@gmail.com>
2021-02-02 12:23:42 -0600

    testing serverless offline

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 // lambda-like handler function
-module.exports.handler = async (event) => {
-  // do stuff...
-};
+// module.exports.handler = async (event) => {
+//   // do stuff...
+// };
+
+module.exports.handler = serverless(app);
